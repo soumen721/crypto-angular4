dd0606842d254580c9cc553ee85dc2c788bc365a
Soumen Choudhury <sou.tcs@gmail.com>
2018-05-02 21:57:21 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { DataService } from './data.service';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  objectKeys = Object.keys;
+  cryptos: any;
+
+  constructor(private _data: DataService) {
+    setInterval(()=> {this.ngOnInit();}, 10000);
+  }
+
+  ngOnInit() {
+    this._data.getPrices()
+      .subscribe(res => {
+        this.cryptos = res;
+        console.log(res);
+      });
+  }
+
+}
